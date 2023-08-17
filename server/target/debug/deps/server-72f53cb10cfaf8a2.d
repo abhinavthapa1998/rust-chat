694bfafa0a14774c5b011274c33240a1de240cd9
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-08-17 22:56:28 +0530

    Rust Chat Setup

---
+++ server/target/debug/deps/server-72f53cb10cfaf8a2.d
@@ -0,0 +1,5 @@
+E:\personal-projects\rust-chat\server\target\debug\deps\server-72f53cb10cfaf8a2.rmeta: src\main.rs
+
+E:\personal-projects\rust-chat\server\target\debug\deps\server-72f53cb10cfaf8a2.d: src\main.rs
+
+src\main.rs:
---
+++ server/target/debug/deps/server-86eea66999be7e3b.d
@@ -0,0 +1,5 @@
+E:\personal-projects\rust-chat\server\target\debug\deps\server-86eea66999be7e3b.rmeta: src\main.rs
+
+E:\personal-projects\rust-chat\server\target\debug\deps\server-86eea66999be7e3b.d: src\main.rs
+
+src\main.rs:
